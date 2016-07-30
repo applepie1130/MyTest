04766ae79aa8b1b816d334404ea8ed37847dea98
KimSungJun <arduous1130@gmail.com>
2016-07-30 16:37:35 +0900

    [20160730] guava, lamdaj 추가

---
+++ MyTest/src/main/java/com/applepie1130/basic/BigdecimalTest.java
@@ -0,0 +1,62 @@
+package com.applepie1130.basic;
+
+import java.math.BigDecimal;
+
+public class BigdecimalTest {
+	
+	public static void main(String[] args) {
+		
+//	    double a = 507.4;
+//	    double b = 226.2;
+//	    System.out.println( "507.4 + 226.2 = " + (a+b) );
+	    
+//	    BigDecimal a = BigDecimal.valueOf(507.4);
+//	    BigDecimal b = BigDecimal.valueOf(226.2);
+//	    System.out.println(a.add(b));
+//		
+//		// <참고>
+//		// 만일 아래와 같이 선언할 경우 733.59999999999996589394868351519107818603515625 값으로 double생성자를 사용하면 정확한 결과가 나오지 않게 된다.
+//		BigDecimal a = new BigDecimal(507.4);
+//	    BigDecimal b = new BigDecimal(226.2);
+//	    System.out.println(a.add(b));
+		
+		
+//	    double a = 2.0;
+//	    double b = 1.1;
+//	    System.out.println( "2.0 - 1.1 = " + (a-b) );
+		
+		float a = (float) 2.2;
+		float b = 2.2f;
+		double c = 2.2;
+		
+		System.out.println(b == c);
+//	    BigDecimal a = BigDecimal.valueOf(2.0);
+//	    BigDecimal b = BigDecimal.valueOf(1.1);
+//	    System.out.println( "a - b = " + a.subtract(b) );
+		
+//		System.out.println(1.0 / 3.0);
+	    
+	    
+	    
+		
+//		// 1번문제
+//		double a = 1.0;
+//		double b = 0.1;
+//		
+//		for ( int i=0; i<5; i++ ) {
+//			a += b;
+//			System.out.println( a );
+//		}
+//		
+//		
+//		// 1번문제
+//		BigDecimal bigA = BigDecimal.valueOf(1.0);
+//		BigDecimal bigB = BigDecimal.valueOf(0.1);
+//		
+//		for ( int i=0; i<5; i++ ) {
+//			bigA = bigA.add(bigB);
+//			System.out.println( bigA );
+//		}
+//		
+	}
+}
--- MyTest/src/main/java/com/applepie1130/basic/Test.java
+++ MyTest/src/main/java/com/applepie1130/basic/Test.java
@@ -1,11 +1,24 @@
 package com.applepie1130.basic;
 
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+
 public class Test {
 
 	@org.junit.Test
 	public void test () {
 		int diff = 1101111 ^ 1101111;
-
 		System.out.println(diff);
+	}
+	
+	@org.junit.Test	
+	public void useMapService() {
+		Map<String, Object> map = Maps.newHashMap();
+		List<String> list = Lists.newArrayList();
+		LinkedHashMap<String, Object> lhmap = Maps.newLinkedHashMap();
 	}
 }
